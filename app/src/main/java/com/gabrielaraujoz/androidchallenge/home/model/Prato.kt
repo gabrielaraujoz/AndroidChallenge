3bada4372bc666727c54f0219b480819e6adb488
gabrielaraujoz <garaujo.eng@gmail.com>
2020-11-08 18:36:09 -0300

    Adicionando card_view e grid layout

--- app/src/main/java/com/gabrielaraujoz/androidchallenge/home/model/Prato.kt
+++ app/src/main/java/com/gabrielaraujoz/androidchallenge/home/model/Prato.kt
@@ -3,6 +3,6 @@ package com.gabrielaraujoz.androidchallenge.home.model
 import android.graphics.drawable.Drawable
 import android.media.Image
 
-data class Prato (var nomePrato: String, var imagem: Image  ) {
+data class Prato (var nomePrato: String, var imagem: Image, var descricao: String  ) {
 
 }
