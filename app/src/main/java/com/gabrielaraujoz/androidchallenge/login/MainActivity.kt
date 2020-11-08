8e3dcd214bc498c5dfad97940f1e2ca5b83869ae
gabrielaraujoz <garaujo.eng@gmail.com>
2020-11-08 11:59:34 -0300

    consegui adicionar o back button na barra superior da RegisterActivity

--- app/src/main/java/com/gabrielaraujoz/androidchallenge/login/MainActivity.kt
+++ app/src/main/java/com/gabrielaraujoz/androidchallenge/login/MainActivity.kt
@@ -1,11 +1,23 @@
-package com.gabrielaraujoz.androidchallenge
+package com.gabrielaraujoz.androidchallenge.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gabrielaraujoz.androidchallenge.R
+import com.gabrielaraujoz.androidchallenge.register.RegisterActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonRegister = findViewById<MaterialButton>(R.id.btnRegister)
+        val buttonLogin = findViewById<MaterialButton>(R.id.btnLogin)
+
+        buttonRegister.setOnClickListener(){
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/gabrielaraujoz/androidchallenge/register/RegisterActivity.kt
+++ app/src/main/java/com/gabrielaraujoz/androidchallenge/register/RegisterActivity.kt
@@ -1,11 +1,15 @@
 package com.gabrielaraujoz.androidchallenge.register
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gabrielaraujoz.androidchallenge.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
